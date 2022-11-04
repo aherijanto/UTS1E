f85607cb3be7287044226c7b59c711c252edbc6b
bistique <aherijanto@gmail.com>
2022-11-04 03:24:52 -0500

    baru

--- src/JawabanNoTiga.java
+++ src/JawabanNoTiga.java
@@ -1,5 +1,7 @@
 public class JawabanNoTiga {
     public static void main(String[] args) {
         System.out.println("hello kelas 1E");
+        System.out.println("udah ada yang baru");
+        int[] angka = new int[4];
     }
 }
